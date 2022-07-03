ffc910b6a79bcd7ee2891b1cff18f94f5b494d87
liamorales <morales.lia.1990@gmail.com>
2022-07-02 22:52:49 -0500

    adding

---
+++ Homework3.js
@@ -0,0 +1,13 @@
+//question 1
+
+let ages = [3,9,23,64,2,8,28,93];
+let ages = age.length;
+
+let text = "<ul>";
+ages.forEach(myFunction);
+text += "</ul>";
+
+function myFunction(value) {
+    text += "<li>" + value + "</li>";
+}
+
